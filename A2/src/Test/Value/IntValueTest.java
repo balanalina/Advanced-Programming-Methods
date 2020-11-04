7db10bfa51e07e82b3ffec174ff630ea1d1d0e46
Balan Alina <alinabalan16.ab@gmail.com>
2020-11-05 01:10:58 +0200

    value tests

---
+++ A2/src/Test/Value/IntValueTest.java
@@ -0,0 +1,32 @@
+package Test.Value;
+
+import Model.Type.BoolType;
+import Model.Value.IntValue;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class IntValueTest {
+    private IntValue value_0;
+    private IntValue value_17;
+
+    @BeforeEach
+    void setUp() {
+        this.value_0 = new IntValue();
+        this.value_17 = new IntValue(17);
+    }
+
+    @Test
+    void getValue() {
+        assertEquals(0, value_0.getValue());
+        assertEquals(17, value_17.getValue());
+        assertNotEquals(177, value_17.getValue());
+    }
+
+    @Test
+    void getType() {
+        assertEquals(new IntValue(), value_0.getType());
+        assertNotEquals(new BoolType(), value_0.getType());
+    }
+}
