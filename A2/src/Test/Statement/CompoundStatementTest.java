d2846ab40f7328e57f8b58d23735ebfcb368f3b2
Balan Alina <alinabalan16.ab@gmail.com>
2020-11-06 02:49:48 +0200

    finished model

---
+++ A2/src/Test/Statement/CompoundStatementTest.java
@@ -0,0 +1,41 @@
+package Test.Statement;
+
+import ADT.myDictionary;
+import ADT.myList;
+import ADT.myStack;
+import Model.Expression.ValueExpression;
+import Model.ProgramState;
+import Model.Statement.CompoundStatement;
+import Model.Statement.IStatement;
+import Model.Statement.NopStatement;
+import Model.Statement.PrintStatement;
+import Model.Value.BoolValue;
+import Model.Value.Value;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CompoundStatementTest {
+    private CompoundStatement test_statement;
+    private ProgramState state;
+    private IStatement first_stmt;
+    private IStatement second_stmt;
+
+    @BeforeEach
+    void setUp() {
+        this.first_stmt = new NopStatement();
+        this.second_stmt = new PrintStatement(new ValueExpression(new BoolValue(true)));
+        this.test_statement = new CompoundStatement(this.first_stmt, this.second_stmt);
+        this.state = new ProgramState(new myStack<IStatement>(), new myDictionary<String,Value>(), new myList<Value>(), this.test_statement);
+        this.state.getExecution_stack().push(this.test_statement);
+    }
+
+    @Test
+    void execute() {
+        this.test_statement.execute(this.state);
+        assertFalse(this.state.getExecution_stack().empty());
+        assertEquals(this.state.getExecution_stack().pop(), this.first_stmt);
+        assertEquals(this.state.getExecution_stack().pop(), this.second_stmt);
+    }
+}
---
+++ A2/src/Test/Statement/PrintStatementTest.java
@@ -0,0 +1,33 @@
+package Test.Statement;
+
+import ADT.myDictionary;
+import ADT.myList;
+import ADT.myStack;
+import Model.Expression.ValueExpression;
+import Model.ProgramState;
+import Model.Statement.CompoundStatement;
+import Model.Statement.IStatement;
+import Model.Statement.PrintStatement;
+import Model.Value.IntValue;
+import Model.Value.Value;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PrintStatementTest {
+    private PrintStatement test_statement;
+    private ProgramState state;
+
+    @BeforeEach
+    void setUp() {
+        this.test_statement = new PrintStatement(new ValueExpression(new IntValue(6556)));
+        this.state = new ProgramState(new myStack<IStatement>(), new myDictionary<String, Value>(), new myList<Value>(), this.test_statement);
+    }
+
+    @Test
+    void execute() {
+        this.test_statement.execute(this.state);
+        assertEquals(1, this.state.getOut_table().size());
+    }
+}
